30cc67ff6dcd47e4754ea01593bed4566c7ac436
paradox8599 <paradox8599@outlook.com>
2024-07-06 00:35:54 +1000

    some code

---
+++ src/bin/main.rs
@@ -0,0 +1,15 @@
+use eyre::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Ok(())
+    // let client = OscClient::default().await?;
+    // // let oc_arc = Arc::new(client);
+    // // let oc_recv = oc_arc.clone();
+    // client
+    //     .listen(|msg| {
+    //         println!("{:?}", msg);
+    //     })
+    //     .await;
+    // Ok(())
+}
---
+++ src/vrchat/mod.rs
@@ -0,0 +1,3 @@
+pub mod events;
+pub use events::{VRChatEvent, Gesture, TrackingType, Upright, Viseme};
+
